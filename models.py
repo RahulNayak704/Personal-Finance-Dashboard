080a98a7080eb8a55dc5775cc67f2e00fd428ef4
Rahul Nayak <77767992+RahulNayak704@users.noreply.github.com>
2024-10-10 00:31:51 -0400

    models.py

---
+++ models.py
@@ -0,0 +1,14 @@
+from . import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    description = db.Column(db.String(200))
